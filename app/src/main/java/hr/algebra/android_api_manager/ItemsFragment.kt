dd14552cb08f540181ad1852c740ca829ed764b4
Marko Galiman <61890185+mgaliman@users.noreply.github.com>
2022-02-09 13:03:37 +0100

    Update03
    Finished exercise03

---
+++ app/src/main/java/hr/algebra/android_api_manager/ItemsFragment.kt
@@ -0,0 +1,18 @@
+package hr.algebra.android_api_manager
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+class ItemsFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_items, container, false)
+    }
+}
